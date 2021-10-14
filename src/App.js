c018dfccfecd3dacf319fbe17d38f42641c1cc8c
DESKTOP-NCUSJM3\patry <patrykmendel8@gmail.com>
2021-10-14 15:27:49 +0200

    handle resposive container and squares

--- src/App.js
+++ src/App.js
@@ -1,38 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useEffect} from 'react'
-import Square from './Square';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useRef, useState } from "react";
+import Square from "./Square";
 
 function App() {
+  const [containerSize, setContainerSize] = useState([]);
+
+  // Reference to get container size, and make it always a square
+  const container = useRef();
 
   // Array to map through, and render squares
   const squares = [];
-
   const fillArray = (counter) => {
     for (let i = 0; i < counter; i++) {
       squares.push(i);
     }
   };
 
-// Rendering squares
+  // Rendering squares
   const renderSqaures = () => {
     fillArray(25);
     return squares.map((item) => <Square />);
   };
-
+  //
+  useEffect(() => {
+    fillArray();
+    setContainerSize([window.innerHeight, window.innerWidth]);
+  }, []);
 
   useEffect(() => {
-    fillArray()
-  },[])
+    const handleResize = () => {
+      setContainerSize([window.innerHeight, window.innerWidth]);
+    };
 
+    window.onresize = handleResize;
+  });
+
+  const containerStyle = () => {
+    if (window.innerWidth > window.innerHeight) {
+      return {
+        width: containerSize[0],
+        height: "100%",
+      };
+    } else
+      return {
+        height: containerSize[1],
+        width: "100%",
+      };
+  };
+
+  // const containerStyle = {
+  //   width: containerSize[0],
+  //   height: '100%'
+  // }
 
   return (
-    <div className="App">
-      <div className="container" >
+    <div className="app">
+      <div ref={container} style={containerStyle()} className="container">
         {renderSqaures()}
       </div>
     </div>
-  
   );
 }
 
--- src/Square.js
+++ src/Square.js
@@ -3,8 +3,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 function Square() {
+
+
+    const squareStyle = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+        padding: 10,
+        overflow: 'hidden',
+        width: '20%',
+        height: '20%',
+    
+    }
+
     return (
-        <div>
+        <div style = {squareStyle} className="square" >
             <FontAwesomeIcon style = {{fontSize: '2.5em'}} icon={faCoffee} />
             <p>Tekst</p>
         </div>
